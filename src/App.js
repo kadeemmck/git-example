61e0a30982737c3eaccd0821cb53f275c068b346
COR Students <corstudents@Matts-MacBook-Pro-57.local>
2019-02-18 11:13:43 -0500

    fixed header bug

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+      <div>
       <h1>this a kadeem branch</h1>
+      <h2>i fixed the header 😅</h2>
+      </div>
     );
   }
 }
